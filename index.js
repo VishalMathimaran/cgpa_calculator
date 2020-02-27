d5713e8d82fdf9eb4076c9f93a568f87487b3f43
VishalTennyson <vishalvarshni.23@gmail.com>
2020-02-27 20:05:02 +0530

    done

--- index.js
+++ index.js
@@ -2,39 +2,41 @@ const express = require("express");
 const app = express();
 const path = require("path")
 const bodyParser = require('body-parser')
-const port = 8080 || process.env.PORT;
-let cgpa = 0
-let gpas = []
-let home = []
+const port = 8080 || process.env.PORT;        //Use  environment variable PORT or default 8080
+let cgpa = 0  //cummulative gpa value
+let gpas = []   //array of gpas
+let current = [] //array of current semester grades with subject
+
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({
-  extended: true
+  extended: true      //enable bodyparser to use req.body
 }));
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');    //set view engine to use ejs
 
 
 app.get("/", function(req, res) {
   res.render("index.ejs", {
-    home: home,
+    //render the ejs file index.ejs with all three values eventhough only cgpa is used for further enhancement
+    current: current,
     cgpa: cgpa,
     gpas: gpas
   })
 })
-
+// <-----------------------------------------Get the CGPA With given information --------------------------------------------------->
 app.post("/", function(req, res) {
   console.log("Post method")
-  let data = req.body
-  home = [data]
+  let data = req.body     //store the request into data
+  current = [data]
   let gpa = 0
   let cgpa = 0
   let total = 0
-  home.forEach(function(element) {
-    if (typeof(element.grade) === "object") {
+  current.forEach(function(element) {
+    if (typeof(element.grade) === "object") {       //if there are more than one subject
       for (i = 0; i < element.grade.length; i++) {
         gpa = gpa + Number(element.grade[i]) * Number(element.credit[i])
         total = total + parseInt(element.credit[i])
       }
-    } else {
+    } else {      //if there is only one subject
       gpa = element.credit * element.grade + gpa
       total = total+ Number(element.credit)
     }
@@ -51,19 +53,19 @@ app.post("/", function(req, res) {
   })
   cgpa = tempCGPA / tempTotal
   res.render("index.ejs", {
-    home: home,
+    current: current,
     cgpa: cgpa,
     gpas: gpas
   })
 })
-
+// <-----------------------------------------Clears all the previous information --------------------------------------------------->
 app.get('/refresh', function(req, res) {
-  home = []
+  current = []
   gpas = []
-  console.log(home)
+  console.log(current)
   cgpa = 0
   res.render("index.ejs", {
-    home: home,
+    current: current,
     cgpa: cgpa,
     gpas: gpas
   })
--- public/action.js
+++ public/action.js
@@ -1,107 +1,34 @@
-// alert("Hello Worls")
-count = 1
+count = 1   //Count variable for indexing
 $(document).ready(function() {
-  $(".hidden").css({
-    "display": "none"
-  });
-  $(".hidden1").css({
-    "display": "none"
-  });
-  $(".hidden2").css({
-    "display": "none"
-  });
-  $(".hidden3").css({
-    "display": "none"
-  });
-  $("#add").click(function() {
-
-    input = jQuery('   <tr>\
-        <th scope="row">'+(count+1)+'</th>\
-        <td><input type="text" name="name" id="name">\
-          <p class="hidden">Enter name</p>\
-        </td>\
-\
-        <td><input type="text" name="code" id="subcode">\
-          <p class="hidden1">Enter subject code</p>\
-        </td>\
-        <td> <select class="custom-select" id="inputGroupSelect01" name="credit">\
-            <option selected>Choose...</option>\
-            <option value=1>One</option>\
-            <option value=2>Two</option>\
-            <option value=3>Three</option>\
-            <option value=4>Four</option>\
-          </select>\
-          <p class="hidden2">Choose credit</p>\
-        </td>\
-        <td> <select class="custom-select" id="inputGroupSelect02" name="grade">\
-            <option selected>Choose...</option>\
-            <option value=10>O</option>\
-            <option value=9>A+</option>\
-            <option value=8>A</option>\
-            <option value=7>B+</option>\
-            <option value=6>B</option>\
-            <option value=0>RA</option>\
-          </select>\
-          <p class="hidden3">Choose grade</p>\
-        </td>\
-      </tr>')
-    count++;
-    // button = jQuery('<button type="button" name="button" id="button">Click me</button>')
-
-    jQuery('#formID').append(input);
-    // jQuery('#formID').append(button);
-  });
-
-});
-
-function validate() {
-  val = true
-
-  console.log("choose" + $("#inputGroupSelect01").val())
-  if ($("#name").val() === '') {
-    $(".hidden").css({
-      "display": "block",
-      "color": "red"
-    });
-    val = false
-  } else {
-    $(".hidden").css({
-      "display": "none"
-    });
-  }
-  if ($("#subcode").val() === '') {
-    $(".hidden1").css({
-      "display": "block",
-      "color": "red"
-    });
-    val = false
-  } else {
-    $(".hidden1").css({
-      "display": "none"
-    });
-  }
-  if ($("#inputGroupSelect01").val() === 'Choose...') {
-    $(".hidden2").css({
-      "display": "block",
-      "color": "red"
-    });
-    val = false
-  } else {
-    $(".hidden2").css({
-      "display": "none"
-    });
-  }
-  if ($("#inputGroupSelect02").val() === 'Choose...') {
-    $(".hidden3").css({
-      "display": "block",
-      "color": "red"
-    });
-val = false
-  } else {
-    $(".hidden3").css({
-      "display": "none"
-    });
-  }
-
-  return val;
-}
+  //When the document is ready add a click event
+      $("#add").click(function() {
+          count++;
+          input = jQuery(`<tr>\
+      <th scope="row">` + count + `</th>
+      <td><input type="text" name="name" id="name" required oninvalid="this.setCustomValidity('Enter Course Name Here')" oninput="this.setCustomValidity('')">
+      </td>
+      <td><input type="text" name="code" id="subcode" required oninvalid="this.setCustomValidity('Enter Subject Code Here')" oninput="this.setCustomValidity('')">
+      </td>
+      <td> <select class="custom-select" id="inputGroupSelect01" name="credit" required oninvalid="this.setCustomValidity('Choose Credit Here')" oninput="this.setCustomValidity('')">
+          <option selected value = "">Choose...</option>
+          <option value=1>One</option>
+          <option value=2>Two</option>
+          <option value=3>Three</option>
+          <option value=4>Four</option>
+        </select>
+      </td>
+      <td> <select class="custom-select" id="inputGroupSelect02" name="grade" required oninvalid="this.setCustomValidity('Choose your Grade Here')" oninput="this.setCustomValidity('')">
+          <option selected value = "">Choose...</option>
+          <option value=10>O</option>
+          <option value=9>A+</option>
+          <option value=8>A</option>
+          <option value=7>B+</option>
+          <option value=6>B</option>
+          <option value=0>RA</option>
+        </select>
+      </td>
+    </tr>`)
+            //Append the new course column to the form
+            jQuery('#formID').append(input);
+          });
+      });
